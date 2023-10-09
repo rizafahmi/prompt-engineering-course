b8126a286a78727a0f951c975230e8d9799e9a60
Riza Fahmi <rizafahmi@gmail.com>
2023-10-09 17:49:37 +0700

    Example hallucination

---
+++ 7-hallucination.js
@@ -0,0 +1,22 @@
+import { getCompletion } from "./helpers.js";
+
+const prompt = `Tell me about CleverGlass Smart Glasses by Olike`;
+
+const result = await getCompletion(prompt);
+console.log(result);
+
+/* Jawaban per 9 oktober 2023. Jawaban bisa berubah kedepannya. Jawaban dibawah ini adalahh salah satu contoh halusinasi yang cukup meyakinkan.
+
+CleverGlass Smart Glasses by Olike are a pair of advanced wearable glasses that offer a range of features and functionalities. These smart glasses are designed to provide users with a hands-free and immersive experience while staying connected to their digital world.
+
+One of the key features of CleverGlass Smart Glasses is the built-in display, which allows users to view information and content directly in front of their eyes. The display is transparent, ensuring that users can still see their surroundings while accessing digital content. This feature makes the glasses ideal for augmented reality (AR) applications, as it overlays digital information onto the real world.
+
+The glasses also come with a built-in camera, enabling users to capture photos and videos effortlessly. This camera can be used for various purposes, such as recording memories, live streaming, or even for video conferencing. Additionally, CleverGlass Smart Glasses support voice commands, allowing users to control the glasses and access features without the need for physical interaction.
+
+Another notable feature of these smart glasses is their compatibility with smartphones. Users can connect their smartphones to the glasses via Bluetooth, enabling them to receive notifications, calls, and messages directly on the glasses' display. This feature ensures that users can stay connected and informed without having to constantly check their phones.
+
+CleverGlass Smart Glasses are also designed with comfort in mind. They are lightweight and have an adjustable frame, ensuring a comfortable fit for extended wear. The glasses are also equipped with a long-lasting battery, allowing users to use them throughout the day without worrying about frequent recharging.
+
+Overall, CleverGlass Smart Glasses by Olike offer a convenient and immersive way to access digital content and stay connected while on the go. With their advanced features and comfortable design, these smart glasses are a great choice for tech enthusiasts and individuals looking for a hands-free digital experience.
+
+ */
