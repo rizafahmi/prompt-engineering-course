449d3bf22ca8d892b7074daed6a51dfbe84fea97
Riza Fahmi <rizafahmi@gmail.com>
2023-10-19 11:10:47 +0700

    Finish setup fastify

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import Fastify from "fastify";
+import view from "@fastify/view";
+import nunjucks from "nunjucks";
+import path from "path";
+
 const app = Fastify({ logger: true });
+app.register(view, {
+  engine: { nunjucks },
+  viewExt: "njk.html",
+  root: path.join(path.resolve(), "templates"),
+});
 
 app.get("/", function (req, res) {
-  return { hello: "world" };
+  res.view("index", { text: "Hello, world!" });
 });
 
 try {
   await app.listen({ port: 3000 });
-} catch {
+} catch (err) {
   app.log.error(err);
   process.exit(1);
 }
